a5cebb1076b85d84179a8b946f6c6becc18c8fed
charicame <79563467+charicame@users.noreply.github.com>
2021-03-19 00:49:02 +0900

    Add files via upload

---
+++ ABC/100/B.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm> //max,min
+#include<iomanip> //setprecision,tuple
+#include<vector>  //vector
+#include<numeric> //iota
+#include<map> //map
+#include<set> //set
+using namespace std;
+using lint = long long;
+#define INF 2147483647
+#define PI acos(-1)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main(){
+    int d,n;cin>>d>>n;
+    lint ans=pow(100,d);
+    if(n!=100)cout<<ans*n<<endl;
+    else cout<<ans*101<<endl;
+    return 0;
+}
---
+++ ABC/100/C.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm> //max,min
+#include<iomanip> //setprecision,tuple
+#include<vector>  //vector
+#include<numeric> //iota
+#include<map> //map
+#include<set> //set
+using namespace std;
+using lint = long long;
+#define INF 2147483647
+#define PI acos(-1)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main(){
+    int n;cin>>n;
+    lint ans=0;
+    rep(i,n){
+        int a;cin>>a;
+        while(a%2==0){
+            ans++;a/=2;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
