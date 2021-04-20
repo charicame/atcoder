0a944c3ffdf7eb560666635a009d51178cf62d8f
charicame <79563467+charicame@users.noreply.github.com>
2021-04-20 23:50:20 +0900

    Add files via upload

---
+++ ABC/029/B.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm> //max,min
+#include<iomanip> //setprecision,tuple
+#include<vector>  //vector
+#include<numeric> //iota
+#include<map> //map
+#include<set> //set
+#include<sstream> //stringstream(hex 16,dec 10,oct 8,bilset 2)
+#include<list> //list 
+using namespace std;
+using lint = long long;
+#define INF 2147483647
+#define PI acos(-1)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main(){
+    int cnt=0;
+    rep(i,12){
+        string s;cin>>s;
+        rep(j,s.size())if(s[j]=='r'){cnt++;break;}
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
---
+++ ABC/029/C.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm> //max,min
+#include<iomanip> //setprecision,tuple
+#include<vector>  //vector
+#include<numeric> //iota
+#include<map> //map
+#include<set> //set
+#include<sstream> //stringstream(hex 16,dec 10,oct 8,bilset 2)
+#include<list> //list 
+using namespace std;
+using lint = long long;
+#define INF 2147483647
+#define PI acos(-1)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int n;
+
+void maker(string s){
+    if(s.size()==n){
+        cout<<s<<endl;
+        return;
+    }
+    maker(s+'a');
+    maker(s+'b');
+    maker(s+'c');
+}
+
+int main(){
+    cin>>n;
+    maker("");
+    return 0;
+}
